387d6ddbdca26f5aef2f8133c841e2b7d7dd4bfb
Om <oplanke@gmail.com>
2023-09-23 21:51:28 +0530

    sql tables

--- api/tabels.sql
+++ api/tabels.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-	id INTEGER PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	user_name VARCHAR,
 	display_picture VARCHAR,
 	created_at TIMESTAMP,
@@ -7,21 +7,21 @@ CREATE TABLE users (
 );
 
 CREATE TABLE authors (
-	id INTEGER PRIMARY KEY,
-	user_id INTEGER FOREIGN KEY REFERENCES users(id),
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users(id),
 	author_name VARCHAR,
 	display_picture VARCHAR,
  	created_at TIMESTAMP
 );
 
 CREATE TABLE user_followers (
-	user_id INTEGER FOREIGN KEY REFERENCES users(id),
-	follwing_id INTEGER FOREIGN KEY REFERENCES users(id),
-  PRIMARY KEY (user_id, follwing_id)
+	user_id INTEGER REFERENCES users(id),
+	follwing_id INTEGER REFERENCES users(id),
+	PRIMARY KEY (user_id, follwing_id)
 );
 
 CREATE TABLE books (
-	id INTEGER PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	isbn bigint UNIQUE,
 	title VARCHAR,
 	subtitle VARCHAR,
@@ -29,74 +29,74 @@ CREATE TABLE books (
 	rating INTEGER,
 	cover_page VARCHAR,
  	published_at TIMESTAMP,
-	upvote_user_ids INTEGER,
-	downvote_user_ids INTEGER,
 	created_at TIMESTAMP
 );
 
 CREATE TABLE book_authors (
-	id INTEGER PRIMARY KEY,
-	book_id INTEGER FOREIGN KEY REFERENCES books(id),
-	author_id INTEGER FOREIGN KEY REFERENCES authors(id),
-  PRIMARY KEY (books_author_id, authors_id)
+	id SERIAL PRIMARY KEY,
+	book_id INTEGER REFERENCES books(id),
+	author_id INTEGER REFERENCES authors(id),
+	PRIMARY KEY (books_author_id, authors_id)
 );
 
 CREATE TABLE book_reads (
-	user_id INTEGER FOREIGN KEY REFERENCES users(id),
-	book_id INTEGER FOREIGN KEY REFERENCES books(id),
+	user_id INTEGER REFERENCES users(id),
+	book_id INTEGER REFERENCES books(id),
 	created_at TIMESTAMP
-  PRIMARY KEY (user_id, book_id)
+	PRIMARY KEY (user_id, book_id)
 );
 
 CREATE TABLE book_votes (
-  user_id INTEGER FOREIGN KEY REFERENCES users(id),
-	book_id INTEGER FOREIGN KEY REFERENCES books(id),
-  PRIMARY KEY (user_id, book_id),
-  upvote BOOLEAN
+	user_id INTEGER REFERENCES users(id),
+	book_id INTEGER REFERENCES books(id),
+	PRIMARY KEY (user_id, book_id),
+	upvote BOOLEAN
 );
 
 CREATE TABLE reviews (
-  user_id INTEGER FOREIGN KEY REFERENCES users(id),
-  book_id INTEGER,
-  PRIMARY KEY (user_id, book_id)
-  rating INTEGER,
-  title VARCHAR,
-  content text,
-  upvote_user_ids INTEGER,
-  downvote_user_ids INTEGER,
-  created_at TIMESTAMP
+	user_id INTEGER REFERENCES users(id),
+	book_id INTEGER,
+	PRIMARY KEY (user_id, book_id),
+	rating INTEGER,
+	title VARCHAR,
+	content text,
+	created_at TIMESTAMP
 );
 
+-- didnt work
 CREATE TABLE review_votes (
-  FOREIGN KEY (review_user_id, book_id) REFERENCES reviews(user_id, book_id),
-  user_id INTEGER FOREIGN KEY REFERENCES user(id),
-  PRIMARY KEY (user_id, review_user_id, book_id),
-  upvote BOOLEAN
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id),
+    review_user_id INTEGER,
+    book_id INTEGER,
+    upvote BOOLEAN,
+    FOREIGN KEY (review_user_id, book_id) REFERENCES reviews(user_id, book_id),
+    created_at TIMESTAMP
 );
 
 CREATE TABLE comments (
-  FOREIGN KEY (review_user_id, book_id) REFERENCES reviews(user_id, book_id),
-  user_id INTEGER FOREIGN KEY REFERENCES users(id),
-  PRIMARY KEY (user_id, review_user_id, book_id),
-  title VARCHAR,
-  content VARCHAR,
-  created_at TIMESTAMP
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id),
+	review_user_id INTEGER,
+	book_id INTEGER,
+    FOREIGN KEY (review_user_id, book_id) REFERENCES reviews(user_id, book_id),
+	title VARCHAR,
+	content VARCHAR,
+	created_at TIMESTAMP
 );
 
 CREATE TABLE posts (
-  id INTEGER PRIMARY KEY,
-  user_id INTEGER FOREIGN KEY REFERENCES users(id),
-  book_id INTEGER ,
-  title VARCHAR,
-  content text,
-  upvote_user_ids INTEGER,
-  downvote_user_ids INTEGER,
-  created_at TIMESTAMP
+    id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users(id),
+	book_id INTEGER REFERENCES books(id),
+	title VARCHAR,
+	content text,
+	created_at TIMESTAMP
 );
 
-CREATE TABLE book_votes (
-  user_id INTEGER FOREIGN KEY REFERENCES users(id),
-	post_id INTEGER FOREIGN KEY REFERENCES posts(id),
-  PRIMARY KEY (user_id, book_id),
-  upvote BOOLEAN
+CREATE TABLE post_votes (
+	user_id INTEGER REFERENCES users(id),
+	post_id INTEGER REFERENCES posts(id),
+	PRIMARY KEY (user_id, post_id),
+	upvote BOOLEAN
 );
